165b27e9941e9df8ff22f92f7958a78403237a53
Nathan Osman <nathan@quickmediasolutions.com>
2018-03-31 00:57:47 -0700

    Add database connection type.

---
+++ db/config.go
@@ -0,0 +1,6 @@
+package db
+
+// Config stores the database connection information.
+type Config struct {
+	Filename string
+}
---
+++ db/conn.go
@@ -0,0 +1,37 @@
+package db
+
+import (
+	"github.com/jinzhu/gorm"
+	"github.com/sirupsen/logrus"
+
+	// Use an SQLite database internally
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
+)
+
+// Conn represents a connection to the database.
+type Conn struct {
+	*gorm.DB
+	log *logrus.Entry
+}
+
+// New creates a new database connection and initializes it.
+func New(cfg *Config) (*Conn, error) {
+	g, err := gorm.Open("sqlite3", cfg.Filename)
+	if err != nil {
+		return nil, err
+	}
+	c := &Conn{
+		DB:  g,
+		log: logrus.WithField("context", "db"),
+	}
+	return c, nil
+}
+
+// Migrate performs all database migrations.
+func (c *Conn) Migrate() error {
+	c.log.Info("performing migrations...")
+	return c.AutoMigrate(
+		&Site{},
+		&Outage{},
+	).Error
+}
