5a39faf6b12c58338425a49fe26df48781a86e76
Nathan Osman <nathan@quickmediasolutions.com>
2018-04-05 20:34:06 -0700

    Add createadmin command.

--- cmd/msm/main.go
+++ cmd/msm/main.go
@@ -6,8 +6,10 @@ import (
 	"os/signal"
 	"syscall"
 
+	"github.com/howeyc/gopass"
 	"github.com/nathan-osman/my-site-monitor/db"
 	"github.com/nathan-osman/my-site-monitor/server"
+	"github.com/nathan-osman/numbles/cache/src/github.com/howeyc/gopass"
 	"github.com/urfave/cli"
 )
 
@@ -51,6 +53,50 @@ func main() {
 			Value:  ":8000",
 			EnvVar: "SERVER_ADDR",
 			Usage:  "server address",
+		},
+	}
+	app.Commands = []cli.Command{
+		{
+			Name:  "createadmin",
+			Usage: "create an admin user",
+			Action: func(c *cli.Context) error {
+
+				// Connect to the database
+				conn, err := db.New(&db.Config{
+					Host:     c.String("db-host"),
+					Port:     c.Int("db-port"),
+					Name:     c.String("db-name"),
+					User:     c.String("db-user"),
+					Password: c.String("db-password"),
+				})
+				if err != nil {
+					return err
+				}
+				defer conn.Close()
+
+				// Prompt for the username
+				var username string
+				fmt.Print("Username? ")
+				fmt.Scanln(&username)
+
+				// Prompt for the password, hiding the input
+				fmt.Print("Password? ")
+				p, err := gopass.GetPasswd()
+				if err != nil {
+					return err
+				}
+
+				// Create a user and set their password
+				u := &db.User{
+					Username: username,
+					IsAdmin:  true,
+				}
+				if err := u.SetPassword(string(p)); err != nil {
+					return err
+				}
+
+				return nil
+			},
 		},
 	}
 	app.Action = func(c *cli.Context) error {
