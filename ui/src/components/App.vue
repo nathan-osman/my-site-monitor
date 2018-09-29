fea01507eb8a27fbcd93ed09577a855bcc02a8f0
Nathan Osman <nathan@quickmediasolutions.com>
2018-09-29 13:31:17 -0700

    Add working template.

---
+++ ui/src/components/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <AppHeader />
+
+    <div class="container">
+      Hello, world!
+    </div>
+  </div>
+</template>
+
+<script>
+import AppHeader from './AppHeader'
+
+export default {
+  name: 'App',
+  components: {
+    AppHeader
+  }
+}
+</script>
---
+++ ui/src/components/AppHeader.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="AppHeader">
+    <div class="container">
+      <h2>
+        <a href="#">My Site Monitor</a>
+      </h2>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+  .AppHeader {
+    background-color: #aa3939;
+    margin-bottom: 16px;
+    padding: 16px 0;
+  }
+  .AppHeader h2 {
+    font-size: 20pt;
+    font-weight: normal;
+    margin: 0;
+  }
+  .AppHeader a {
+    color: #d46a6a;
+    text-decoration: none;
+    transition: color 200ms ease;
+  }
+  .AppHeader a:hover {
+    color: #ffaaaa;
+    transition: color 200ms ease;
+  }
+</style>
