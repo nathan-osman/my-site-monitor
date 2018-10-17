cb59c321a6b988b8c92c2554f114838a8ec8b9e2
Nathan Osman <nathan@quickmediasolutions.com>
2018-10-16 23:50:06 -0700

    Add duration property to outage model.

--- ui/app/models/outage.js
+++ ui/app/models/outage.js
@@ -1,8 +1,15 @@
 import DS from 'ember-data';
+import { computed } from '@ember/object';
+import moment from 'moment';
 
 export default DS.Model.extend({
   startTime: DS.attr('date'),
   endTime: DS.attr('date'),
   description: DS.attr('string'),
-  site: DS.belongsTo('site')
+  site: DS.belongsTo('site'),
+
+  duration: computed('startTime', 'endTime', function() {
+    let startTime = moment(this.get('startTime'));
+    return startTime.diff(this.get('endTime') || moment());
+  })
 });
